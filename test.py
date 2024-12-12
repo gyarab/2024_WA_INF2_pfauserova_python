2a5216dd0d22634efa616c20673983878bd0eaf3
pfaub <pfau.bara@gmail.com>
2024-12-12 09:49:47 +0100

    prime

--- test.py
+++ test.py
@@ -2,15 +2,22 @@ def fibonacci(n):
     if not isinstance(n,int) or n < 0:
         raise ValueError("Input must be a positive integer")
     elif n == 0:
-        return [0]
+        return 0
     elif n == 1:
-        return [0, 1]
+        return 1
+    else:   
+        return fibonacci(n-1) + fibonacci(n-2)
     
-    fib_sequence = [0, 1]
-    for i in range(2, n):
-        next_value = fib_sequence[-1] + fib_sequence[-2]
-        fib_sequence.append(next_value)
-    
-    return fib_sequence
+
 if __name__ == "__main__":  
-    print(fibonacci(0)) 
+    print(fibonacci(3)) 
+
+    def is_prime(num):
+        if not isinstance(num, int) or num < 2:
+            raise ValueError("Input must be an integer greater than 1")
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+
+    print(is_prime(78))  
